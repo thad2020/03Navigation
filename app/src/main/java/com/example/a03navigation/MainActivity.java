fd4213c4db6eaad8d1ba5d362a9c35dab3982cad
thad2020 <73698744+thad2020@users.noreply.github.com>
2022-03-04 08:45:38 +0100

    Opgave 5 færdig:)

--- app/src/main/java/com/example/a03navigation/MainActivity.java
+++ app/src/main/java/com/example/a03navigation/MainActivity.java
@@ -1,26 +1,54 @@
 package com.example.a03navigation;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button button;
+    EditText editText;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button.findViewById(R.id.button);
+        button = findViewById(R.id.button);
+        editText = findViewById(R.id.inputField);
+        textView = findViewById(R.id.textView1);
 
-    button.setOnClickListener(v ->  {
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null && bundle.containsKey("result")) {
+            String text = bundle.getString("result");
+            textView.setText(text);
+        }
+
+        ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(), result -> {
+                    Intent intent = result.getData();
+                    if (intent != null) {
+                        intent.putExtra("textView", textView.getText().toString());
+                        textView.setText(intent.getStringExtra("result"));
+                    }
+                    else
+                        textView.setText("Jeg er null, miv");
+                });
+
+        button.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, MainActivity2.class);
-            startActivity(intent);
-    });
+            intent.putExtra("inputField", editText.getText().toString());
+            activityResultLauncher.launch(intent);
+        });
+
     }
 }
--- app/src/main/java/com/example/a03navigation/MainActivity2.java
+++ app/src/main/java/com/example/a03navigation/MainActivity2.java
@@ -2,13 +2,43 @@ package com.example.a03navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
+
+    TextView textView;
+    EditText editText;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        textView = findViewById(R.id.textView2);
+        editText = findViewById(R.id.inputField2);
+        button = findViewById(R.id.button2);
+
+        Bundle bundle = getIntent().getExtras();
+
+        if (bundle != null && bundle.containsKey("inputField")) {
+            String text = bundle.getString("inputField");
+            textView.setText(text);
+        }
+
+        button.setOnClickListener(v -> {
+            goBack();
+        });
+    }
+
+    public void goBack() {
+        Intent intent = new Intent();
+        intent.putExtra("result", editText.getText().toString());
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
